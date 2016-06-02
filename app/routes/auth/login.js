b3f1c434ced14a45ff858091d592b4a4b9b7fcb9
Ryan Yogan <ryanyogan@gmail.com>
2016-06-02 10:45:53 -0500

    added in toast messages

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -4,6 +4,7 @@ const { Route, inject } = Ember;
 
 export default Route.extend({
   session: inject.service(),
+  flashMessages: inject.service(),
 
   actions: {
     doLogin() {
@@ -11,7 +12,18 @@ export default Route.extend({
       this.get('session')
         .authenticate(
           'authenticator:blabber', user.email, user.password
-        );
+        ).then(() => {
+          this.get('flashMessages').success('Logged in!');
+        }).catch((response) => {
+          const { errors } = response;
+
+          if (errors.mapBy('code').indexOf(401) >= 0) {
+            this.get('flashMessages')
+              .danger('There was a problem with your username or plassword.');
+          } else {
+            this.get('flashMessages').danger('Server Error');
+          }
+        });
     }
   },
 
